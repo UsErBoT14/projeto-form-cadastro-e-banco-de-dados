91851ecfc8f409a28bc20c056a66d1b2520ff867
Nicolas <77795700+UsErBoT14@users.noreply.github.com>
2021-01-27 14:15:04 -0300

    Add files via upload

---
+++ comen.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+<title>Avaliação formulário</title>
+</head>
+
+<body>
+<center>
+<h3>Muito obrigado pela avaliação!</h3><br><br>
+<br>
+<p2>contato: nicolas123ficticio@gmail.com</p2>
+
+<a href="index.php">Voltar para página inicial...</a>
+
+<?php
+
+$coment = $_GET['comentario'];
+
+echo "<br>Seu comentário foi enviado: $coment";
+
+?>
+</center>
+</body>
+
+</html>
